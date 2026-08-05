8cce2dd4270f342f48ea9905f2ddc1ccbcedb1b4
Moshoeshoe Koali <koalim@wasco.co.ls>
2026-08-05 19:27:47 +0200

    Add accounting tutorials 6-8 (ledger, trial balance, cash book, petty cash) with revision notes and worked questions

--- src/data/accounting-tutorials.ts
+++ src/data/accounting-tutorials.ts
@@ -4,6 +4,9 @@ import { accountingTutorial2 } from './accounting-tutorial-2';
 import { accountingTutorial3 } from './accounting-tutorial-3';
 import { accountingTutorial4 } from './accounting-tutorial-4';
 import { accountingTutorial5 } from './accounting-tutorial-5';
+import { accountingTutorial6 } from './accounting-tutorial-6';
+import { accountingTutorial7 } from './accounting-tutorial-7';
+import { accountingTutorial8 } from './accounting-tutorial-8';
 
 export const accountingTutorials: Booklet[] = [
   accountingTutorial1,
@@ -11,4 +14,7 @@ export const accountingTutorials: Booklet[] = [
   accountingTutorial3,
   accountingTutorial4,
   accountingTutorial5,
+  accountingTutorial6,
+  accountingTutorial7,
+  accountingTutorial8,
 ];
